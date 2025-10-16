c6862e1303a47529e2b8d7ac01bce545b6dade07
Matthew Clifford <matthewclifford@hotmail.co.uk>
2025-10-16 12:34:44 +0200

    updated Dockerfile and removed port and host in main.py as app was not deploying correctly on Render.

--- spotify-profile-backend/main.py
+++ spotify-profile-backend/main.py
@@ -19,4 +19,5 @@ app.include_router(search.router)
 app.include_router(user.router)
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    # uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app)
